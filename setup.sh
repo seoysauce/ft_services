54cdfcc1638b4b077768d19b1c9f304bc32d2f46
Your Name <du0928@naver.com>
2021-04-12 17:53:30 +0900

    commit 2

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
-Docker pull alpine:latest
+#rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc
 
+#brew install minikube
+minikube start --driver=virtualbox
+minikube status
+#Docker pull alpine:latest

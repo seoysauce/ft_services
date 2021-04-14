adc53f31ca70111e2454ad234502835afd8f29d0
Your Name <du0928@naver.com>
2021-04-14 14:47:58 +0900

    commit 3

--- setup.sh
+++ setup.sh
@@ -3,4 +3,16 @@
 #brew install minikube
 minikube start --driver=virtualbox
 minikube status
-#Docker pull alpine:latest
+
+echo "metalLB setup start"
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+cd srcs
+MINIKUBE_IP=$(minikube ip)
+sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" metallb-config_format.yaml > metallb-config.yaml
+kubectl apply -f metallb-config.yaml
+#kubectl apply -f metallb-config.yaml >> $LOG_PATH
+
+echo "nginx setup start"
+docker build -t alpine-nginx nginx/

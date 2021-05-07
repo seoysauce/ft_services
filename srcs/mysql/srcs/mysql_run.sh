0651fe88ebc9a68158c3a23f3297aadb19cf159c
Your Name <du0928@naver.com>
2021-05-07 22:00:20 +0900

    commit 11

--- srcs/mysql/srcs/mysql_run.sh
+++ srcs/mysql/srcs/mysql_run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 mv /tmp/mariadb /etc/init.d/
-#chmod 444 /tmp/mariadb-server.cnf
+chmod 444 /tmp/mariadb-server.cnf
 mv /tmp/mariadb-server.cnf /etc/my.cnf.d/
 
-#touch /run/openrc/softlevel
-rc-service mariadb setup
+rc-status && touch /run/openrc/softlevel && rc-service mariadb setup # rc-status 이게 없으면 실행이 안됨 touch 이게 없으면 rc-service가 실행이 안됨
 
 if [[ ! -d /run/mysqld ]]; then
 	mkdir /run/mysqld
@@ -13,5 +12,3 @@ fi
 
 mysqld --user=root --bootstrap < /tmp/mysql_init.sql
 mysqld --user=root --console
-
-bash

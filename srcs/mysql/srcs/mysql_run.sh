f6ac582bea1317fd19b84f20d66c7320989094f0
Your Name <du0928@naver.com>
2021-05-07 15:46:23 +0900

    commit 10

--- srcs/mysql/srcs/mysql_run.sh
+++ srcs/mysql/srcs/mysql_run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 mv /tmp/mariadb /etc/init.d/
-chmod 444 /tmp/mariadb-server.cnf
+#chmod 444 /tmp/mariadb-server.cnf
 mv /tmp/mariadb-server.cnf /etc/my.cnf.d/
 
-rc-status && touch /run/openrc/softlevel && rc-service mariadb setup
+#touch /run/openrc/softlevel
+rc-service mariadb setup
 
 if [[ ! -d /run/mysqld ]]; then
 	mkdir /run/mysqld
@@ -12,3 +13,5 @@ fi
 
 mysqld --user=root --bootstrap < /tmp/mysql_init.sql
 mysqld --user=root --console
+
+bash

4b3c92e6c294501cca905628b8ffb0764fc42841
Woong <mac@MACui-MacBook-Air.local>
2021-05-17 18:18:11 +0900

    tmp

--- srcs/wordpress/wordpress_run.sh
+++ srcs/wordpress/wordpress_run.sh
@@ -1,7 +1,8 @@
 /usr/bin/supervisord -c /etc/supervisord.conf
 
 #nginx -g "daemon off;"
-#/bin/sh
+#/usr/sbin/php-fpm7 -F
+#telegraf
 
 # Run PHP specifying port and file location
 #/usr/bin/php -S 0.0.0.0:5050 -t /www/wordpress

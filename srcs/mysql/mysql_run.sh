c9d616889ec2681e145b06847202699e6d22abd0
Your Name <you@example.com>
2021-05-18 15:25:04 +0900

    tmp

--- srcs/mysql/mysql_run.sh
+++ srcs/mysql/mysql_run.sh
@@ -1,13 +1,9 @@
-# nginx
-chmod 444 /tmp/mariadb-server.cnf
-
-# connect wordpress
-mv /tmp/mariadb-server.cnf /etc/my.cnf.d/
-
-# rc-status 이게 없으면 실행이 안됨 touch 이게 없으면 rc-service가 실행이 안됨
-rc-status && touch /run/openrc/softlevel && rc-service mariadb setup
-
-mkdir /run/mysqld
-
-mysqld --user=root --bootstrap < /tmp/mysql_init.sql
-mysqld --user=root --console
+rc default
+/usr/bin/telegraf &
+/etc/init.d/mariadb setup
+rc-service mariadb start
+mysql -u root mysql < /tmp/mysql_init
+mysql -u root my_db < wordpress_db.sql
+rc-service mariadb stop
+/usr/bin/mysqld_safe
+supervisord -c /etc/supervisord.conf

ada704a35b82c4dde5e05a2d789365cdb5d35840
Your Name <you@example.com>
2021-05-20 18:05:38 +0900

    last 1

--- srcs/phpmyadmin/phpmyadmin_run.sh
+++ srcs/phpmyadmin/phpmyadmin_run.sh
@@ -1,18 +1,8 @@
-PHPMYADMIN_DATA="$(wget https://www.phpmyadmin.net/home_page/version.txt -q -O-)"
-PHPMYADMIN_URL="$(echo $PHPMYADMIN_DATA | cut -d ' ' -f 3)"
-PHPMYADMIN_VER="$(echo $PHPMYADMIN_DATA | cut -d ' ' -f 1)"
-
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar -xf phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages/* /www/
 rm phpMyAdmin-5.0.2-all-languages.tar.gz
 
 mv config.inc.php www/config.inc.php
-chmod 644 www/config.inc.php
-#chmod 777 -R /www/
 
 supervisord -c /etc/supervisord.conf
-
-#nginx -g "daemon off;"
-
-#php -S 0.0.0.0:5000 -t /www/
--- srcs/wordpress/wordpress_run.sh
+++ srcs/wordpress/wordpress_run.sh
@@ -1,8 +1,1 @@
 supervisord -c /etc/supervisord.conf
-
-#nginx -g "daemon off;"
-#/usr/sbin/php-fpm7 -F
-#telegraf
-
-# Run PHP specifying port and file location
-#/usr/bin/php -S 0.0.0.0:5050 -t /www/wordpress

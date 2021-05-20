eecf6f90cdd7570b4844373dc05018aea0d1e8b1
Your Name <you@example.com>
2021-05-20 23:03:34 +0900

    last commit

--- setup.sh
+++ setup.sh
@@ -16,24 +16,23 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/metallb-config_format.yaml > srcs/metallb-config.yaml
 kubectl apply -f srcs/metallb-config.yaml
-kubectl apply -f metallb-config.yaml >> $LOG_PATH
 
 echo "build image"
 docker build -t alpine-nginx srcs/nginx/
-#docker build -t alpine-mysql srcs/mysql/
-#docker build -t alpine-wordpress srcs/wordpress/
-#docker build -t alpine-phpmyadmin srcs/phpmyadmin/
-#docker build -t alpine-influxdb srcs/influxdb/
-#docker build -t alpine-grafana srcs/grafana/
-#docker build -t alpine-ftps srcs/ftps/
+docker build -t alpine-mysql srcs/mysql/
+docker build -t alpine-wordpress srcs/wordpress/
+docker build -t alpine-phpmyadmin srcs/phpmyadmin/
+docker build -t alpine-influxdb srcs/influxdb/
+docker build -t alpine-grafana srcs/grafana/
+docker build -t alpine-ftps srcs/ftps/
 
 echo "create object"
 kubectl apply -f ./srcs/nginx/nginx.yaml
-#kubectl apply -f ./srcs/mysql/mysql.yaml
-#kubectl apply -f ./srcs/wordpress/wordpress.yaml
-#kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
-#kubectl apply -f ./srcs/influxdb/influxdb.yaml
-#kubectl apply -f ./srcs/grafana/grafana.yaml
-#kubectl apply -f ./srcs/ftps/ftps.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/influxdb/influxdb.yaml
+kubectl apply -f ./srcs/grafana/grafana.yaml
+kubectl apply -f ./srcs/ftps/ftps.yaml
 
 kubectl get all
